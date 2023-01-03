7460eba1c6533be82fc09a5bcf52b1313d6859ca
jasonferrier <jason.ferrier@gmail.com>
2023-01-03 09:20:31 -0700

    Added JSON parsing of response

--- libib_title_scraper.py
+++ libib_title_scraper.py
@@ -1,18 +1,37 @@
 from bs4 import BeautifulSoup
 
 import requests
+import json
 
+# GeorgeHooverArchitect Collection
 BASE_URL = "https://www.libib.com/u/ghoover"
-# page = requests.get(URL)
 
-# This API endpoint returns a JSON response
+# This API endpoint returns a JSON response with the results
+# Search Help documentation
+ENDPOINT_SEARCH = BASE_URL + '/search'
+
+# This API endpoint returns a JSON string with the results
+ENDPOINT_GET_ITEMS = BASE_URL + '/get-account-items'
+
+
 GET_ITEMS = BASE_URL + "/get-account-items"
-form_data = {'library_id': 77535, 'offset': 0}
+
+PARAMS_ITEMS_PER_PAGE = 50
+PARAMS_PAGE_NUMBER = 1
+# This is hardcoded to look at the first page of results
+#  The default number of items per page is 50, so offset is calculated:
+#   offset = page * 50
+form_data = {'library_id': 77535, 'offset': PARAMS_ITEMS_PER_PAGE * PARAMS_PAGE_NUMBER}
+# e.g. For page two, use the following line instead of above
+# form_data = {'library_id': 77535, 'offset': 50}
+
 
 data = requests.post(GET_ITEMS, data=form_data)
 
 if data:
     print('Success!')
-    print(data.content)
+    # Convert string to Python dict
+    results = json.loads(data.content)
+    print(results)
 else:
     print('Not Found.')
